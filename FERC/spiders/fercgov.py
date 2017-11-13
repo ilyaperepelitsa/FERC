d728456ffbe04ae9cd998a776a853c0531d7641a
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2017-11-13 12:42:34 -0500

    search thing working

--- FERC/spiders/fercgov.py
+++ FERC/spiders/fercgov.py
@@ -17,13 +17,13 @@ class FercgovSpider(scrapy.Spider):
     start_urls = ['https://elibrary.ferc.gov/idmws/search/fercgensearch.asp']
     # docket = docket
     docstart = 0
-    doccoutner = 200
+    doccounter = 200
     docslimit = 200
     # dockets = ["CP16-17", "CP15-500"]
-    # dockets = ["CP16-17"]
-    dockets = []
-    search = "pipeline"
-    # search = ""
+    dockets = ["CP15-500"]
+    # dockets = []
+    # search = "pipeline"
+    search = ""
 
 
     def parse(self, response):
@@ -33,62 +33,120 @@ class FercgovSpider(scrapy.Spider):
             for docket in self.dockets:
 
                 query = FormRequest.from_response(response,
-                                        formdata = {
-                                                    # "FROMdt" : "",
-                                                    # "TOdt" : "",
-                                                    "firstDt" : "1/1/1904",
-                                                    "LastDt" : "12/31/2037",
-                                                    "DocsStart" : str(self.docstart),
-                                                    "DocsLimit" : str(self.docslimit),
-                                                    # "SortSpec" : "filed_date desc accession_num asc",
-                                                    # "datefield" : "filed_date",
-                                                    # "dFROM" : "10/08/2017",
-                                                    # "dTO" : "11/08/2017",
-                                                    # "dYEAR" : "1",
-                                                    # "dMONTH" : "1",
-                                                    # "dDAY" : "1",
-                                                    "date" : "All",
-                                                    # "NotCategories" : "submittal,issuance",
-                                                    "category" : "submittal,issuance",
-                                                    # "category" : "submittal",
-                                                    # "category" : "issuance",
-                                                    "libraryall" : "electric, hydro, gas, rulemaking, oil, general",
-                                                    "docket" : str(docket),
-                                                    "subdock_radio" : "all_subdockets",
-                                                    # "class" : "999",
-                                                    # "type" : "999",
-                                                    "textsearch" : str(self.search),
-                                                    "description" : "description",
-                                                    "fulltext" : "fulltext",
-                                                    "DocsCount" : str(self.doccoutner)},
+                                        # formdata = {
+                                        #             # "FROMdt" : "",
+                                        #             # "TOdt" : "",
+                                        #             "firstDt" : "1/1/1904",
+                                        #             "LastDt" : "12/31/2037",
+                                        #             "DocsStart" : str(self.docstart),
+                                        #             "DocsLimit" : str(self.docslimit),
+                                        #             # "SortSpec" : "filed_date desc accession_num asc",
+                                        #             # "datefield" : "filed_date",
+                                        #             # "dFROM" : "10/08/2017",
+                                        #             # "dTO" : "11/08/2017",
+                                        #             # "dYEAR" : "1",
+                                        #             # "dMONTH" : "1",
+                                        #             # "dDAY" : "1",
+                                        #             "date" : "All",
+                                        #             # "NotCategories" : "submittal,issuance",
+                                        #             "category" : "submittal,issuance",
+                                        #             # "category" : "submittal",
+                                        #             # "category" : "issuance",
+                                        #             "libraryall" : "electric, hydro, gas, rulemaking, oil, general",
+                                        #             "docket" : str(docket),
+                                        #             "subdock_radio" : "all_subdockets",
+                                        #             # "class" : "999",
+                                        #             # "type" : "999",
+                                        #             "textsearch" : str(self.search),
+                                        #             "description" : "description",
+                                        #             "fulltext" : "fulltext",
+                                        #             "DocsCount" : str(self.doccounter)},
+
+                                            formdata = {
+                                                        "FROMdt" : "",
+                                                        "TOdt" : "",
+                                                        "firstDt" : "1/1/1904",
+                                                        "LastDt" : "12/31/2037",
+                                                        "DocsStart" : str(self.docstart),
+                                                        "DocsLimit" : str(self.docslimit),
+                                                        "SortSpec" : "filed_date desc accession_num asc",
+                                                        "datefield" : "filed_date",
+                                                        "dFROM" : "10/08/2017",
+                                                        "dTO" : "11/08/2017",
+                                                        "dYEAR" : "1",
+                                                        "dMONTH" : "1",
+                                                        "dDAY" : "1",
+                                                        "date" : "All",
+                                                        # "NotCategories" : "submittal,issuance",
+                                                        "category" : "submittal,issuance",
+                                                        # "category" : "submittal",
+                                                        # "category" : "issuance",
+                                                        "libraryall" : "electric, hydro, gas, rulemaking, oil, general",
+                                                        "docket" : str(docket),
+                                                        "subdock_radio" : "all_subdockets",
+                                                        "class" : "999",
+                                                        "type" : "999",
+                                                        "textsearch" : str(self.search),
+                                                        "description" : "description",
+                                                        "fulltext" : "fulltext",
+                                                        "DocsCount" : str(self.doccounter)},
                                         # clickdata={'name': 'commit'},
                                         callback=self.parse_query, dont_filter = True)
                 query.meta["DocsStart"] = str(self.docstart)
                 query.meta["docket"] = str(docket)
                 query.meta["textsearch"] = str(self.search)
-                query.meta["DocsCount"] = str(self.doccoutner)
+                query.meta["DocsCount"] = str(self.doccounter)
                 query.meta["DocsLimit"] = str(self.docslimit)
 
                 yield query
-                open_in_browser(response)
+                # open_in_browser(response)
         else:
 
 
             query = FormRequest.from_response(response,
+                                    # formdata = {
+                                    #             # "FROMdt" : "",
+                                    #             # "TOdt" : "",
+                                    #             "firstDt" : "1/1/1904",
+                                    #             "LastDt" : "12/31/2037",
+                                    #             "DocsStart" : str(self.docstart),
+                                    #             "DocsLimit" : str(self.docslimit),
+                                    #             # "SortSpec" : "filed_date desc accession_num asc",
+                                    #             # "datefield" : "filed_date",
+                                    #             # "dFROM" : "10/08/2017",
+                                    #             # "dTO" : "11/08/2017",
+                                    #             # "dYEAR" : "1",
+                                    #             # "dMONTH" : "1",
+                                    #             # "dDAY" : "1",
+                                    #             "date" : "All",
+                                    #             # "NotCategories" : "submittal,issuance",
+                                    #             "category" : "submittal,issuance",
+                                    #             # "category" : "submittal",
+                                    #             # "category" : "issuance",
+                                    #             "libraryall" : "electric, hydro, gas, rulemaking, oil, general",
+                                    #             "docket" : "",
+                                    #             "subdock_radio" : "all_subdockets",
+                                    #             # "class" : "999",
+                                    #             # "type" : "999",
+                                    #             "textsearch" : str(self.search),
+                                    #             "description" : "description",
+                                    #             "fulltext" : "fulltext",
+                                    #             "DocsCount" : str(self.doccounter)},
+
                                     formdata = {
-                                                # "FROMdt" : "",
-                                                # "TOdt" : "",
+                                                "FROMdt" : "",
+                                                "TOdt" : "",
                                                 "firstDt" : "1/1/1904",
                                                 "LastDt" : "12/31/2037",
                                                 "DocsStart" : str(self.docstart),
                                                 "DocsLimit" : str(self.docslimit),
-                                                # "SortSpec" : "filed_date desc accession_num asc",
-                                                # "datefield" : "filed_date",
-                                                # "dFROM" : "10/08/2017",
-                                                # "dTO" : "11/08/2017",
-                                                # "dYEAR" : "1",
-                                                # "dMONTH" : "1",
-                                                # "dDAY" : "1",
+                                                "SortSpec" : "filed_date desc accession_num asc",
+                                                "datefield" : "filed_date",
+                                                "dFROM" : "10/08/2017",
+                                                "dTO" : "11/08/2017",
+                                                "dYEAR" : "1",
+                                                "dMONTH" : "1",
+                                                "dDAY" : "1",
                                                 "date" : "All",
                                                 # "NotCategories" : "submittal,issuance",
                                                 "category" : "submittal,issuance",
@@ -97,27 +155,27 @@ class FercgovSpider(scrapy.Spider):
                                                 "libraryall" : "electric, hydro, gas, rulemaking, oil, general",
                                                 "docket" : "",
                                                 "subdock_radio" : "all_subdockets",
-                                                # "class" : "999",
-                                                # "type" : "999",
+                                                "class" : "999",
+                                                "type" : "999",
                                                 "textsearch" : str(self.search),
                                                 "description" : "description",
                                                 "fulltext" : "fulltext",
-                                                "DocsCount" : str(self.doccoutner)},
+                                                "DocsCount" : str(self.doccounter)},
                                     # clickdata={'name': 'commit'},
                                     callback=self.parse_query, dont_filter = True)
             query.meta["DocsStart"] = str(self.docstart)
             query.meta["docket"] = ""
             query.meta["textsearch"] = str(self.search)
-            query.meta["DocsCount"] = str(self.doccoutner)
+            query.meta["DocsCount"] = str(self.doccounter)
             query.meta["DocsLimit"] = str(self.docslimit)
 
             yield query
-            open_in_browser(response)
+            # open_in_browser(response)
         # pass
 
     def parse_query(self, response):
 
-        # open_in_browser(response)
+        open_in_browser(response)
 
         page_rows = response.xpath('//tr[@bgcolor and not(@bgcolor="navy")]').extract()
         next_pages = response.xpath('//a[text()="NextPage"]').extract()
@@ -125,7 +183,7 @@ class FercgovSpider(scrapy.Spider):
         docstart = int(response.meta["DocsStart"])
         docket = response.meta["docket"]
         search = response.meta["textsearch"]
-        doccouter = int(response.meta["DocsCount"])
+        doccounter = int(response.meta["DocsCount"])
         docslimit = int(response.meta["DocsLimit"])
 
         # next_pages = LinkExtractor(allow=(),
@@ -137,22 +195,52 @@ class FercgovSpider(scrapy.Spider):
 
         # yield({"pew" : len(page_rows)})
         if len(next_pages) > 0:
-            docstart += doccouter
-            docslimit += doccouter
+            docstart += doccounter
+            docslimit += doccounter
 
             new_query = FormRequest(url="https://elibrary.ferc.gov/idmws/search/results.asp",
+                # formdata = {
+                #             "firstDt" : "1/1/1904",
+                #             "LastDt" : "12/31/2037",
+                #             "DocsStart" : str(docstart),
+                #             "DocsLimit" : str(docslimit),
+                #             "date" : "All",
+                #             "category" : "submittal,issuance",
+                #             "libraryall" : "electric, hydro, gas, rulemaking, oil, general",
+                #             "docket" : str(docket),
+                #             "textsearch" : search,
+                #             "subdock_radio" : "all_subdockets",
+                #             "DocsCount" : str(doccounter)},
+
                 formdata = {
+                            "FROMdt" : "",
+                            "TOdt" : "",
                             "firstDt" : "1/1/1904",
                             "LastDt" : "12/31/2037",
                             "DocsStart" : str(docstart),
                             "DocsLimit" : str(docslimit),
                             "date" : "All",
+                            "SortSpec" : "filed_date desc accession_num asc",
+                            "datefield" : "filed_date",
+                            "dFROM" : "10/08/2017",
+                            "dTO" : "11/08/2017",
+                            "dYEAR" : "1",
+                            "dMONTH" : "1",
+                            "dDAY" : "1",
+                            "date" : "All",
+                            # "NotCategories" : "submittal,issuance",
                             "category" : "submittal,issuance",
+                            # "category" : "submittal",
+                            # "category" : "issuance",
                             "libraryall" : "electric, hydro, gas, rulemaking, oil, general",
                             "docket" : str(docket),
-                            "textsearch" : search,
                             "subdock_radio" : "all_subdockets",
-                            "DocsCount" : str(doccouter)},
+                            "class" : "999",
+                            "type" : "999",
+                            "textsearch" : str(search),
+                            "description" : "description",
+                            "fulltext" : "fulltext",
+                            "DocsCount" : str(doccounter)},
 
                 # formdata = {
                 #             "FROMdt" : "",
@@ -187,7 +275,7 @@ class FercgovSpider(scrapy.Spider):
             new_query.meta["DocsStart"] = str(docstart)
             new_query.meta["docket"] = str(docket)
             new_query.meta["textsearch"] = str(search)
-            new_query.meta["DocsCount"] = str(doccouter)
+            new_query.meta["DocsCount"] = str(doccounter)
             new_query.meta["DocsLimit"] = str(docslimit)
 
             yield new_query
@@ -208,10 +296,10 @@ class FercgovSpider(scrapy.Spider):
         # yield {"pew2" : self.update_settings}
         # parse_query
 
-        yield {"pew2" : [len(page_rows), docket, docstart, doccouter]}
+        yield {"pew2" : [len(page_rows), docket, docstart, doccounter]}
 
 
-        open_in_browser(response)
+        # open_in_browser(response)
 
 # 15 + 167
 #
